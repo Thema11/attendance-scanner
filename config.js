11f81f0a72ee0c2a263f0f34eac22b0e1ae7203f
Thema11 <mvhpth8jhd@privaterelay.appleid.com>
2026-06-12 06:31:39 +0300

    Update config.js تعديل عنوان النشر

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  لا تشاركي هذا الملف مع أحد
 // ══════════════════════════════════════════════════════════
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzmt60L6fwO9tq0LCrwpmpH3bFGiRPrcxIbigKMtCShJxuN8B2Td_uR1JQb-oswzsi_AQ/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxkaDyOPyohXAoKWP9NuJYuRTv9_Gmqu2LdGfw_IvQB7sWxlZhr8NMdXx2ip5KcJJDKzg/exec',
   // كلمات مرور منفصلة لكل دور
   PASSWORDS: {
     admin    : 'Admin@2025',   // ← غيّري كلمة مرور المسؤول
