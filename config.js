9dcf49069cc237882f76b0fceeee33fb2b774107
Thema11 <mvhpth8jhd@privaterelay.appleid.com>
2026-06-12 11:12:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  لا تشاركي هذا الملف مع أحد
 // ══════════════════════════════════════════════════════════
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxkaDyOPyohXAoKWP9NuJYuRTv9_Gmqu2LdGfw_IvQB7sWxlZhr8NMdXx2ip5KcJJDKzg/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzJsUCr-Mt3rvnBVCbWrjY5in62y5A1E5riTUUzX9dn52oHPNy3QoaVYP9rreCdf69GpA/exec',
   // كلمات مرور منفصلة لكل دور
   PASSWORDS: {
     admin    : 'Admin@2025',   // ← غيّري كلمة مرور المسؤول
